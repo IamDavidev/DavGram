aba35e0974340454c14a9f7bf2454d18350dd8fb
L-David-T <davidlez2mgk@gmail.com>
2022-01-09 22:38:07 -0600

    🚀

--- src/hooks/useTrendingPhotos.js
+++ src/hooks/useTrendingPhotos.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import {GetTrendingPhotos} from '../services/GetTrendingphotos';
+import { GetTrendingPhotos } from '../services/GetTrendingphotos';
 
 const useTrendingPhotos = () => {
+  const [loading, setLoading] = useState(true);
   const [trending, setTrending] = useState([]);
   useEffect(() => {
     GetTrendingPhotos().then((res) => {
       setTrending(res);
     });
-  },[Search]);
+  }, [loading]);
+  console.log(trending);
 
   return trending;
 };
